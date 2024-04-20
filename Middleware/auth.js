5574204f1e9de7667337075c9bdccc675d161974
Kaustubh <kaustubhr2001@gmail.com>
2024-04-20 23:23:58 +0530

    some fixes

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -4,7 +4,7 @@ const Employee = require("../Models/Employees.Models");
 
 const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req?.headers?.authorization?.split(" ")[1];
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
     }
